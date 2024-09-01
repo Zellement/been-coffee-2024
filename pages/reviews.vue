f0764ff10bfe4fdc1dd8c4765818d12503b4489b
Dan Farrow <dan@zellement.com>
2024-09-01 14:39:51 +0100

    more bits

--- pages/reviews.vue
+++ pages/reviews.vue
@@ -3,13 +3,16 @@
         <div class="container container-px flex flex-col items-center py-8">
             <page-title title="Reviews" class="mb-4" />
             <!-- <h1 class="font-riverside text-3xl">Team</h1> -->
-            <div class="flex flex-col items-center gap-4">
+            <div class="flex flex-col items-center gap-4 text-center">
                 <p class="">What's that old saying?</p>
 
-                <p class="font-krete text-lg">
+                <p class="font-krete text-xl">
                     "Don't just take our word for it."
                 </p>
-                <p>Here's some of our reviews from Google and Tripadvisor.</p>
+                <p>
+                    Here's some of our recent reviews from Google and
+                    Tripadvisor.
+                </p>
             </div>
         </div>
         <div class="container container-px flex flex-col gap-16">
