9b6c95a740ca11919d9ba04395ae1988e9b3fa38
Dan Farrow <dan@zellement.com>
2024-09-01 14:49:49 +0100

    add contact form

---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="page pb-16">
+        <div class="container container-px flex flex-col items-center py-8">
+            <page-title title="Contact" class="mb-4" />
+            <p>
+                Reach out to us for any questions, feedback or suggestions. We
+                are always happy to hear from you.
+            </p>
+        </div>
+        <div class="container container-px">
+            <form-contact />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: 'Contact us',
+    meta: [
+        {
+            hid: 'description',
+            name: 'description',
+            content: 'Get in touch with us'
+        }
+    ]
+})
+</script>
